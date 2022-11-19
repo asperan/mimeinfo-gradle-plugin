8c3a2bbe36459047e7aea96bc8b2b8193cfc2143
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-19 16:49:29 +0100

    feat: add stub tasks

---
+++ src/main/kotlin/io/github/asperan/mimeinfo/task/MimeInfoFileWriteTask.kt
@@ -0,0 +1,16 @@
+/*
+Copyright (c) 2022, Alex Speranza
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package io.github.asperan.mimeinfo.task
+
+import org.gradle.api.DefaultTask
+
+/**
+ * Task which prints the MimeInfo files.
+ */
+open class MimeInfoFileWriteTask : DefaultTask()
