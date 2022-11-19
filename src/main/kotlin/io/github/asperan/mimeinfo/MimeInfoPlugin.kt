10a21dc4fb599115bfac15552d3ad4bfe964fa43
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-19 16:52:21 +0100

    feat: register tasks and create extension

--- src/main/kotlin/io/github/asperan/mimeinfo/MimeInfoPlugin.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/MimeInfoPlugin.kt
@@ -8,18 +8,38 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package io.github.asperan.mimeinfo
 
+import io.github.asperan.mimeinfo.extension.MimeInfoExtension
+import io.github.asperan.mimeinfo.task.*
+import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.kotlin.dsl.register
 
 /**
  * A plugin to create and install MIME types in the Linux shared database.
  */
-class MimeInfoPlugin: Plugin<Project> {
+open class MimeInfoPlugin: Plugin<Project> {
     override fun apply(project: Project) {
-        // Register a task
-        project.tasks.register("greeting") { task ->
-            task.doLast {
-                println("Hello from plugin 'io.github.asperan.mimeinfo-gradle-plugin'")
+        with(project) {
+            val extension = extensions.create("mimeinfos", MimeInfoExtension::class.java)
+            val systemCheckTask = tasks.register<SystemCheckTask>("checkSystemIsSupported")
+            val checkMimeCommandsTask = tasks.register<CheckMimeCommandsTask>("checkMimeCommandsTask") {
+                dependsOn(systemCheckTask)
+            }
+            val mimeInfoFileWriteTask = tasks.register<MimeInfoFileWriteTask>("writeMimeInfoFiles") {
+                dependsOn(systemCheckTask)
+            }
+            val mimeInfoInstallTask = tasks.register<MimeInfoInstallTask>("installMimeInfos") {
+                dependsOn(checkMimeCommandsTask, mimeInfoFileWriteTask)
+            }
+            val iconInstallTask = tasks.register<IconInstallTask>("installIcons") {
+                dependsOn(mimeInfoInstallTask)
+            }
+            val mimeDatabaseUpdateTask = tasks.register<MimeDatabaseUpdateTask>("updateMimeDatabase") {
+                dependsOn(mimeInfoInstallTask, iconInstallTask)
+            }
+            tasks.register<DefaultTask>("installMimeTypes") {
+                dependsOn(mimeDatabaseUpdateTask)
             }
         }
     }
