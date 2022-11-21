f4096be8928c79c249332239ff863a367b22b4db
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-21 12:04:38 +0100

    feat: add extension for the plugin

--- src/main/kotlin/io/github/asperan/mimeinfo/extension/MimeInfoExtension.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/extension/MimeInfoExtension.kt
@@ -8,9 +8,32 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package io.github.asperan.mimeinfo.extension
 
-import org.gradle.api.Project
+import io.github.asperan.mimeinfo.context.MimeInfoContext
+import io.github.asperan.mimeinfo.mime.MimeInfoSpecs
+import java.io.File
 
 /**
  * MimeInfo plugin extension.
  */
-open class MimeInfoExtension(project: Project)
+open class MimeInfoExtension {
+    /**
+     * The MimeInfoSpecs with the given file name.
+     */
+    var mimeInfoFiles: Map<File, MimeInfoSpecs> private set
+
+    init {
+        mimeInfoFiles = mapOf()
+    }
+
+    /**
+     * Define a new MimeInfo object in the plugin configuration.
+     *
+     * @param fileName The temporary file name where to save the MimeInfo specification.
+     * @param configuration The configuration to be called on the MimeInfo context.
+     */
+    fun mimeinfo(fileName: String, configuration: MimeInfoContext.() -> Unit) {
+        val mimeInfoContext = MimeInfoContext()
+        mimeInfoContext.configuration()
+        mimeInfoFiles = mimeInfoFiles + (File(fileName) to mimeInfoContext.build())
+    }
+}
