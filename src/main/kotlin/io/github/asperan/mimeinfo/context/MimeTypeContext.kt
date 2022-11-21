3cddd36696e4c56cdecb2ecec96afdbc39857d7c
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-21 17:13:22 +0100

    docs: update MimeTypeContext documentation

--- src/main/kotlin/io/github/asperan/mimeinfo/context/MimeTypeContext.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/context/MimeTypeContext.kt
@@ -4,6 +4,9 @@ import io.github.asperan.mimeinfo.mime.MimeTypeSpecs
 
 /**
  * The context of MimeTypes.
+ *
+ * @param typeClass The class of the Mime type.
+ * @param typeName The name of the type.
  */
 class MimeTypeContext(typeClass: MimeTypeSpecs.Type.MimeClass, typeName: String) : Context<MimeTypeSpecs> {
     private val mimeTypeSpecsBuilder = MimeTypeSpecs.Builder()
