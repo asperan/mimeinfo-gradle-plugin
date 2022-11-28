ba5bc84d1c6cbdff87fd0ebe04e9a62bafb4aca5
Alex Speranza <alex.speranza@studio.unibo.it>
2022-11-28 14:59:55 +0100

    refactor: create functions to modularize plugin application

--- src/main/kotlin/io/github/asperan/mimeinfo/MimeInfoPlugin.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/MimeInfoPlugin.kt
@@ -26,26 +26,36 @@ import org.gradle.kotlin.dsl.register
 open class MimeInfoPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         with(project) {
-            val extension = extensions.create("mimeinfos", MimeInfoExtension::class.java)
-            val systemCheckTask = tasks.register<SystemCheckTask>("checkSystemIsSupported")
-            val checkMimeCommandsTask = tasks.register<CheckMimeCommandsTask>("checkMimeCommandsTask") {
-                dependsOn(systemCheckTask)
-            }
-            val mimeInfoFileWriteTask = tasks.register<MimeInfoFileWriteTask>("writeMimeInfoFiles") {
-                dependsOn(systemCheckTask)
-            }
-            val mimeInfoInstallTask = tasks.register<MimeInfoInstallTask>("installMimeInfos") {
-                dependsOn(checkMimeCommandsTask, mimeInfoFileWriteTask)
-            }
-            val iconInstallTask = tasks.register<IconInstallTask>("installIcons") {
-                dependsOn(mimeInfoInstallTask)
-            }
-            val mimeDatabaseUpdateTask = tasks.register<MimeDatabaseUpdateTask>("updateMimeDatabase") {
-                dependsOn(mimeInfoInstallTask, iconInstallTask)
-            }
-            tasks.register<DefaultTask>("installMimeTypes") {
-                dependsOn(mimeDatabaseUpdateTask)
-            }
+            createExtension()
+            registerPluginTasks()
+        }
+    }
+
+    private fun Project.createExtension() {
+        val extension = extensions.create("mimeinfos", MimeInfoExtension::class.java)
+    }
+
+    private fun Project.registerPluginTasks() {
+        val systemCheckTask = tasks.register<SystemCheckTask>("checkSystemIsSupported")
+        val checkMimeCommandsTask = tasks.register<CheckMimeCommandsTask>("checkMimeCommandsTask") {
+            dependsOn(systemCheckTask)
+        }
+        val mimeInfoFileWriteTask = tasks.register<MimeInfoFileWriteTask>("writeMimeInfoFiles") {
+            dependsOn(systemCheckTask)
+        }
+        val mimeInfoInstallTask = tasks.register<MimeInfoInstallTask>("installMimeInfos") {
+            dependsOn(checkMimeCommandsTask, mimeInfoFileWriteTask)
+        }
+        // TODO: maybe installing icons can be performed before the installation of the mimetype.
+        // TODO: study how to install icons in a sane way.
+        val iconInstallTask = tasks.register<IconInstallTask>("installIcons") {
+            dependsOn(mimeInfoInstallTask)
+        }
+        val mimeDatabaseUpdateTask = tasks.register<MimeDatabaseUpdateTask>("updateMimeDatabase") {
+            dependsOn(mimeInfoInstallTask, iconInstallTask)
+        }
+        tasks.register<DefaultTask>("installMimeTypes") {
+            dependsOn(mimeDatabaseUpdateTask)
         }
     }
 }
