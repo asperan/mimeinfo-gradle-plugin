c5328d041b2e3bed3f53f1f0f3b5597cb8b588f8
Alex Speranza <alex.speranza@studio.unibo.it>
2022-12-03 18:23:59 +0100

    feat: implement the installation of mime types and add tests

---
+++ src/functionalTest/kotlin/io/github/asperan/mimeinfo/tasks/MimeInfoInstallTaskTest.kt
@@ -0,0 +1,62 @@
+package io.github.asperan.mimeinfo.tasks
+
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.gradle.testkit.runner.TaskOutcome
+import kotlin.io.path.readText
+import kotlin.io.path.writeText
+
+class MimeInfoInstallTaskTest : TaskFunctionalTest({
+    val taskName = "installMimeInfos"
+    "The task should install the mime type" {
+        val myCustomType = "x-custom-text"
+        val mimeFilePath = "./my-custom-text.xml"
+        settingsFile.writeText(
+            """
+            plugins {
+                id("io.github.asperan.mimeinfo-gradle-plugin")
+            }    
+            """.trimIndent()
+        )
+
+        buildFile.writeText(
+            """
+            import io.github.asperan.mimeinfo.mime.MimeTypeSpecs
+            mimeinfos {
+                mimeinfo("$mimeFilePath") {
+                    mimetype(MimeTypeSpecs.Type.MimeClass.TEXT, "$myCustomType") {
+                        comment("My custom Text type")
+                        glob("*.cstxt")
+                        globDeleteAll = true
+                    }
+                }
+            }
+            """.trimIndent()
+        )
+        val testFile = projectDir.resolve("test.cstxt")
+        testFile.toFile().writeText("test")
+        val result = runTask(taskName)
+        val outputFile = projectDir.resolve("output.txt")
+        try {
+            result.tasks.all { it.outcome == TaskOutcome.SUCCESS }.shouldBeTrue()
+            withContext(Dispatchers.IO) {
+                ProcessBuilder("/bin/bash", "-c", "xdg-mime query filetype ${testFile.toFile().absolutePath}")
+                    .directory(projectDir.toFile())
+                    .redirectOutput(outputFile.toFile())
+                    .start()
+                    .waitFor()
+            }
+            val output = outputFile.readText()
+            output.trim() shouldBe "text/${myCustomType}"
+        } finally {
+            withContext(Dispatchers.IO) {
+                ProcessBuilder("/bin/bash", "-c", "xdg-mime uninstall $mimeFilePath")
+                    .directory(projectDir.toFile())
+                    .start()
+                    .waitFor()
+            }
+        }
+    }
+})
--- src/main/kotlin/io/github/asperan/mimeinfo/task/MimeInfoInstallTask.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/task/MimeInfoInstallTask.kt
@@ -8,9 +8,49 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package io.github.asperan.mimeinfo.task
 
+import io.github.asperan.mimeinfo.extension.MimeInfoExtension
+import io.github.asperan.mimeinfo.utility.EXTENSION_NAME
+import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Exec
+import org.gradle.kotlin.dsl.get
+import javax.inject.Inject
 
 /**
  * Task for installing a MIME info file.
  */
-open class MimeInfoInstallTask : Exec()
+open class MimeInfoInstallTask : DefaultTask() {
+    init {
+        val mimeinfoFiles = (project.extensions[EXTENSION_NAME] as MimeInfoExtension).mimeInfoFiles.keys
+        val indexes = 1 .. mimeinfoFiles.size
+        val baseDependencies = listOf(this.dependsOn.toTypedArray())
+        this.dependsOn(
+            *mimeinfoFiles
+                .zip(indexes)
+                .map {
+                    project.tasks.register(
+                        "installSingleMimeinfo-${it.second}",
+                        SingleInstallTask::class.java,
+                        baseDependencies,
+                        it.first.absolutePath,
+                    )
+                }
+                .toTypedArray()
+        )
+    }
+
+    /**
+     * Task to install a single mime file.
+     *
+     * @param filePath The path of the file.
+     */
+    open class SingleInstallTask @Inject constructor(baseDependencies: List<Any>, filePath: String) : Exec() {
+        init {
+            this.dependsOn(*baseDependencies.toTypedArray())
+            this.dependsOn(project.tasks.get("writeMimeInfoFiles"))
+            commandLine = listOf(
+                "/bin/bash", "-c",
+                "xdg-mime install --novendor --mode user $filePath"
+            )
+        }
+    }
+}
