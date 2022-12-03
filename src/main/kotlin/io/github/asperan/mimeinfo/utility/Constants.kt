286a244201fcab4ffc3040259e7a7ee161447267
Alex Speranza <alex.speranza@studio.unibo.it>
2022-12-03 12:05:08 +0100

    feat: add Constants module

---
+++ src/main/kotlin/io/github/asperan/mimeinfo/utility/Constants.kt
@@ -0,0 +1,16 @@
+package io.github.asperan.mimeinfo.utility
+
+/**
+ * The `mimeinfo-core` dependency name and version.
+ */
+internal const val MIMEINFO_DEPENDENCY = "io.github.asperan:mimeinfo-core:1.1.0"
+
+/**
+ * The name of the buildscript configuration.
+ */
+internal const val BUILDSCRIPT_CONFIGURATION_NAME = "classpath"
+
+/**
+ * The name of the extension of the plugin.
+ */
+internal const val EXTENSION_NAME = "mimeinfos"
