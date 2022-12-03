5643d8af2dcd75c6f01b9d6107f003d4cc86a05f
Alex Speranza <alex.speranza@studio.unibo.it>
2022-12-03 18:27:06 +0100

    docs: add a comment for the MimeDatabaseUpdateTask no-op

--- src/main/kotlin/io/github/asperan/mimeinfo/task/MimeDatabaseUpdateTask.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/task/MimeDatabaseUpdateTask.kt
@@ -12,5 +12,8 @@ import org.gradle.api.tasks.Exec
 
 /**
  * Task which updates the MIME database.
+ *
+ * This action seems to be not required: executing `xdg-mime install` already update the mime types.
+ * If errors or problems occur, this class will be implemented.
  */
 open class MimeDatabaseUpdateTask : Exec()
