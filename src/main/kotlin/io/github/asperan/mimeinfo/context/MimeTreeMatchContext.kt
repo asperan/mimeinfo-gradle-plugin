1caa83864472d6091bc49bf7641169d9e09a531d
Alex Speranza <alex.speranza@studio.unibo.it>
2023-01-02 11:21:44 +0100

    chore: update license headers

--- src/main/kotlin/io/github/asperan/mimeinfo/context/MimeTreeMatchContext.kt
+++ src/main/kotlin/io/github/asperan/mimeinfo/context/MimeTreeMatchContext.kt
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2022, Alex Speranza
+Copyright (c) 2023, Alex Speranza
 
 This Source Code Form is subject to the terms of the Mozilla Public
 License, v. 2.0. If a copy of the MPL was not distributed with this
